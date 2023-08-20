549babaf74a963826565f9ab4303d57142928c37
YunShu <YunShuEmail@foxmail.com>
2023-08-20 21:07:30 +0800

    feat: add the template code for casvisor website (#1)

---
+++ docusaurus.config.js
@@ -0,0 +1,260 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Casvisor · An open-source security log auditing & RDP, VNC, SSH bastion platform",
+  url: "https://casvisor.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "casbin", // Usually your GitHub org/user name.
+  projectName: "casvisor-website", // Usually your repo name.
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "zh", "fr", "de", "ko", "ru", "ja"],
+  },
+  themeConfig: {
+    metadata: [{name: "Casvisor", content: "An open-source security log auditing & RDP, VNC, SSH bastion platform"}],
+    // algolia: {
+    //   appId: "",
+    //   apiKey: "",
+    //   indexName: "casvisor",
+    //   contextualSearch: true,
+    // },
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    hotjar: {
+      applicationId: "3620236",
+    },
+    navbar: {
+      title: "Casvisor",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/casbin.svg",
+      },
+      // hideOnScroll: true,
+      items: [
+        {
+          type: "doc",
+          docId: "overview",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          label: "Blog",
+          to: "/blog",
+          position: "left",
+        },
+        {
+          label: "Help",
+          to: "/help",
+          position: "left",
+        },
+        {
+          href: "https://casdoor.com",
+          label: "For Enterprise",
+          position: "left",
+        },
+        {
+          href: "https://casdoor.com",
+          label: "Hosting Plan (SaaS)",
+          position: "left",
+        },
+        {
+          type: "localeDropdown",
+          position: "right",
+          dropdownItemsAfter: [
+            {
+              type: "html",
+              value: "<hr style=\"margin: 0.3rem 0;\">",
+            },
+            {
+              to: "https://crowdin.com/project/casvisor-website",
+              label: "Help translate",
+            },
+          ],
+        },
+        {
+          href: "https://github.com/casbin/casvisor",
+          position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
+        },
+        {
+          type: "custom-community",
+          position: "right",
+        },
+        {
+          type: "search",
+          position: "right",
+        },
+        {
+          type: "custom-casdoor",
+          label: "Sign Up",
+          src: "https://casdoor.com",
+          position: "right",
+          className: "casdoor-signup casdoor-link",
+        },
+        {
+          type: "custom-casdoor",
+          label: "Login",
+          src: "https://casdoor.com",
+          position: "right",
+          className: "casdoor-login casdoor-link",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Overview",
+              to: "/docs/overview",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/casbin/casvisor",
+            },
+            {
+              label: "Discord",
+              href: "https://discord.gg/yfKcuHVAHB",
+            },
+            {
+              label: "QQ Group",
+              href: "https://qm.qq.com/cgi-bin/qm/qr?k=TgDE42f-w4V3_Scq-2eX-f3bVEspS_tX&jump_from=webapi&authKey=ZLfUKo0GfAMoKWieUx6oJR37M8Z5HQ1aSy7nGSniims26hA8AoP75tNbnZJXPsBy",
+            },
+            {
+              label: "Wechat Group",
+              href: "https://cdn.casdoor.com/casdoor/resource/built-in/admin/wechat.jpg",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/casbin/casvisor",
+            },
+            {
+              html: `
+              <iframe src="https://ghbtns.com/github-btn.html?user=casbin&repo=casvisor&type=star&count=true&size=large" frameborder="0" scrolling="0" width="170" height="30" title="GitHub">Casvisor</iframe>
+              `,
+            },
+            {
+              html: `
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c539599342b1638ad91188a0aacb7b53";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+`,
+            },
+            {
+              html: "<div class=\"placeholderads\"></div>",
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: "Casbin Logo",
+        src: "img/casbin_min.svg",
+        href: "https://casvisor.org/",
+      },
+      copyright: `Copyright © ${new Date().getFullYear()} Casvisor contributors.`,
+    },
+    prism: {
+      additionalLanguages: ["nginx", "java", "properties", "groovy", "ruby"],
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+    announcementBar: {
+      id: "announcement", // ID of the announcement bar
+      content:
+        "<a target=\"_blank\" rel=\"noreferrer\" href=\"https://casbin.org/\">💖 Looking for an open-source and cross-platform authorization solution that supports ACL, RBAC, ABAC? Learn more about: Casbin</a>",
+      isCloseable: true,
+    },
+    docs: {
+      sidebar: {hideable: true},
+    },
+  },
+  // // https://docusaurus.io/docs/markdown-features/diagrams#configuration
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        // gtag: {
+        //   trackingID: "",
+        // },
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          sidebarCollapsed: false,
+          editUrl: ({locale, docPath}) => {
+            if (locale === "en") {
+              return `https://github.com/casbin/casvisor-website/edit/master/docs/${docPath}`;
+            }
+            if (locale === "zh") {
+              return "https://crowdin.com/project/casvisor-website/zh-CN";
+            }
+            return `https://crowdin.com/project/casvisor-website/${locale}`;
+          },
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: ({locale, blogDirPath, blogPath}) => {
+            if (locale === "en") {
+              return `https://github.com/casbin/casvisor-website/edit/master/${blogDirPath}/${blogPath}`;
+            }
+            if (locale === "zh") {
+              return "https://crowdin.com/project/casvisor-website/zh-CN";
+            }
+            return `https://crowdin.com/project/casvisor-website/${locale}`;
+          },
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+        },
+      },
+    ],
+  ],
+  plugins: ["docusaurus-plugin-sass", "docusaurus-plugin-hotjar"],
+  scripts: [
+    {
+      src: "/js/isMainland.js",
+      async: true,
+    },
+    {
+      src: "/js/tawk.js",
+      async: true,
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import styles from "./index.module.scss";
+import HomepageFeatures from "../components/HomepageFeatures";
+// eslint-disable-next-line unused-imports/no-unused-imports
+import Translate, {translate} from "@docusaurus/Translate";
+import {UserList} from "@site/src/components/UserList";
+import {useWindowSize} from "@docusaurus/theme-common";
+
+function HomepageHeader() {
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title"><Translate>Casvisor</Translate></h1>
+        <br />
+        <p className="hero__subtitle" style={{margin: "0 auto", maxWidth: 700}}>
+          <Translate>
+            An open-source security log auditing & RDP, VNC, SSH bastion platform
+          </Translate>
+        </p>
+        <br />
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            style={{marginTop: "2rem", marginRight: "3rem", marginLeft: "3rem"}}
+            to="/docs/overview">
+            <Translate>Get Started</Translate>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+function Showcase() {
+  return (
+    <div className="hero text--center showcase">
+      <div className="container">
+        <h1><Translate>Who&apos;s using Casbin?</Translate></h1>
+        <p style={{
+          width: "50vw",
+          margin: "auto",
+        }}>
+          <Translate values={{
+            UsersPage: (
+              <Link to="/users">
+                <Translate>check out these apps</Translate>
+              </Link>
+            ),
+          }}>
+            {"Hundreds of projects are using Casbin, from established Fortune 500 companies to hot new startups.If you're curious to see what can be accomplished Casbin, {UsersPage}!"}
+          </Translate>
+        </p>
+        <br /><br />
+        <UserList />
+      </div>
+    </div>
+  );
+}
+
+function OpenCollective() {
+  if (useWindowSize() === "mobile") {
+    return (
+      <iframe title="Sponsors" src="https://opencollective.com/casbin/banner.html" style={{width: "100%", height: "1100px", display: "block"}}></iframe>
+    );
+  } else {
+    return (
+      <iframe title="Sponsors" src="https://opencollective.com/casbin/banner.html" style={{width: "100%", height: "650px", display: "block"}}></iframe>
+    );
+  }
+}
+
+export default function Home() {
+  return (
+    <Layout
+      title={"Casvisor"}
+      description="An open-source security log auditing & RDP, VNC, SSH bastion platform">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+        <Showcase />
+        <OpenCollective />
+      </main>
+    </Layout>
+  );
+}
